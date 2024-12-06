80fd40e4b98648996422acba7337e2d6149d9683
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-12-06 11:26:49 +0800

    Add initial robots and sitemap files with commented-out code

---
+++ template/src/app/robots.ts
@@ -0,0 +1,13 @@
+// import { MetadataRoute } from "next";
+
+// export default function robots(): MetadataRoute.Robots {
+//   return {
+//     rules: [
+//       {
+//         userAgent: "*",
+//         allow: ["/", "/contact", "/pricing"],
+//       },
+//     ],
+//     sitemap: "https://domain.com/sitemap.xml",
+//   };
+// }
---
+++ template/src/app/sitemap.ts
@@ -0,0 +1,14 @@
+// import { MetadataRoute } from "next";
+
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   return [
+//     {
+//       url: `https://domain.com/contact`,
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: `https://domain.com/pricing`,
+//       lastModified: new Date(),
+//     },
+//   ];
+// }
